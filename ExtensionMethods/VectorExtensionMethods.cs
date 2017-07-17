f46760859e7e10a015b58df4cfa47429222e88d9
AVAVT <itsavavt@gmail.com>
2017-07-17 08:37:24 +0700

    New Functions

--- ExtensionMethods/VectorExtensionMethods.cs
+++ ExtensionMethods/VectorExtensionMethods.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public static class VectorExtensionMethods {
+public static class VectorExtensionMethods
+{
 
     public static Vector2 ScaleTo(this Vector2 vector, float magnitude)
     {
@@ -12,42 +13,49 @@ public static class VectorExtensionMethods {
         return vector.normalized * magnitude;
     }
 
-    public static Vector2 xy(this Vector3 v) {
-		return new Vector2(v.x, v.y);
-	}
+    public static Vector2 xy(this Vector3 v)
+    {
+        return new Vector2(v.x, v.y);
+    }
 
-	public static Vector3 WithX(this Vector3 v, float x) {
-		return new Vector3(x, v.y, v.z);
-	}
+    public static Vector3 WithX(this Vector3 v, float x)
+    {
+        return new Vector3(x, v.y, v.z);
+    }
 
-	public static Vector3 WithY(this Vector3 v, float y) {
-		return new Vector3(v.x, y, v.z);
-	}
+    public static Vector3 WithY(this Vector3 v, float y)
+    {
+        return new Vector3(v.x, y, v.z);
+    }
 
-	public static Vector3 WithZ(this Vector3 v, float z) {
-		return new Vector3(v.x, v.y, z);
-	}
+    public static Vector3 WithZ(this Vector3 v, float z)
+    {
+        return new Vector3(v.x, v.y, z);
+    }
 
-	public static Vector2 WithX(this Vector2 v, float x) {
-		return new Vector2(x, v.y);
-	}
-	
-	public static Vector2 WithY(this Vector2 v, float y) {
-		return new Vector2(v.x, y);
-	}
-	
-	public static Vector3 WithZ(this Vector2 v, float z) {
-		return new Vector3(v.x, v.y, z);
-        }
-        
+    public static Vector2 WithX(this Vector2 v, float x)
+    {
+        return new Vector2(x, v.y);
+    }
+
+    public static Vector2 WithY(this Vector2 v, float y)
+    {
+        return new Vector2(v.x, y);
+    }
+
+    public static Vector3 WithZ(this Vector2 v, float z)
+    {
+        return new Vector3(v.x, v.y, z);
+    }
+
     // axisDirection - unit vector in direction of an axis (eg, defines a line that passes through zero)
-	// point - the point to find nearest on line for
-	public static Vector3 NearestPointOnAxis(this Vector3 axisDirection, Vector3 point, bool isNormalized = false)
-	{
-	    if (!isNormalized) axisDirection.Normalize();
-	    var d = Vector3.Dot(point, axisDirection);
-	    return axisDirection * d;
-	}
+    // point - the point to find nearest on line for
+    public static Vector3 NearestPointOnAxis(this Vector3 axisDirection, Vector3 point, bool isNormalized = false)
+    {
+        if (!isNormalized) axisDirection.Normalize();
+        var d = Vector3.Dot(point, axisDirection);
+        return axisDirection * d;
+    }
 
     // lineDirection - unit vector in direction of line
     // pointOnLine - a point on the line (allowing us to define an actual line in space)
@@ -58,5 +66,10 @@ public static class VectorExtensionMethods {
         if (!isNormalized) lineDirection.Normalize();
         var d = Vector3.Dot(point - pointOnLine, lineDirection);
         return pointOnLine + (lineDirection * d);
+    }
+
+    public static int GridDistanceTo(this Vector2 vector, Vector2 target)
+    {
+        return (Mathf.RoundToInt(Mathf.Abs(vector.x - target.x) + Mathf.Abs(vector.y - target.y)));
     }
 }
--- TKUtils.cs
+++ TKUtils.cs
@@ -1,41 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TKUtils : MonoBehaviour{
-	
-	public static GameObject Instantiate(GameObject prefab, GameObject parent){
-		GameObject newObject = Instantiate (prefab, Vector3.zero, Quaternion.identity, parent.transform) as GameObject;
-		newObject.transform.localScale = Vector3.one;
-		if (newObject.transform is RectTransform) {
-			(newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
-		}
-		return newObject;
-	}
+public class TKUtils : MonoBehaviour
+{
 
-	public static GameObject Instantiate(GameObject prefab, Transform parent){
-		GameObject newObject = Instantiate (prefab, Vector3.zero, Quaternion.identity, parent) as GameObject;
-		newObject.transform.localScale = Vector3.one;
-		if (newObject.transform is RectTransform) {
-			(newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
-		}
-		return newObject;
-	}
+    public static GameObject Instantiate(GameObject prefab, GameObject parent)
+    {
+        GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity, parent.transform) as GameObject;
+        newObject.transform.localScale = Vector3.one;
+        if (newObject.transform is RectTransform)
+        {
+            (newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
+        }
+        return newObject;
+    }
 
-	public static T Instantiate<T>(GameObject prefab, GameObject parent){
-		GameObject newObject = Instantiate (prefab, Vector3.zero, Quaternion.identity, parent.transform) as GameObject;
-		newObject.transform.localScale = Vector3.one;
-		if (newObject.transform is RectTransform) {
-			(newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
-		}
-		return newObject.GetComponent<T>();
-	}
+    public static GameObject Instantiate(GameObject prefab, Transform parent)
+    {
+        GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity, parent) as GameObject;
+        newObject.transform.localScale = Vector3.one;
+        if (newObject.transform is RectTransform)
+        {
+            (newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
+        }
+        return newObject;
+    }
 
-	public static T Instantiate<T>(GameObject prefab, Transform parent){
-		GameObject newObject = Instantiate (prefab, Vector3.zero, Quaternion.identity, parent) as GameObject;
-		newObject.transform.localScale = Vector3.one;
-		if (newObject.transform is RectTransform) {
-			(newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
-		}
-		return newObject.GetComponent<T>();
-	}
+    public static T Instantiate<T>(GameObject prefab)
+    {
+        GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+        newObject.transform.localScale = Vector3.one;
+        if (newObject.transform is RectTransform)
+        {
+            (newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
+        }
+        return newObject.GetComponent<T>();
+    }
+
+    public static T Instantiate<T>(GameObject prefab, GameObject parent)
+    {
+        GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity, parent.transform) as GameObject;
+        newObject.transform.localScale = Vector3.one;
+        if (newObject.transform is RectTransform)
+        {
+            (newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
+        }
+        return newObject.GetComponent<T>();
+    }
+
+    public static T Instantiate<T>(GameObject prefab, Transform parent)
+    {
+        GameObject newObject = Instantiate(prefab, Vector3.zero, Quaternion.identity, parent) as GameObject;
+        newObject.transform.localScale = Vector3.one;
+        if (newObject.transform is RectTransform)
+        {
+            (newObject.transform as RectTransform).anchoredPosition3D = Vector3.zero;
+        }
+        return newObject.GetComponent<T>();
+    }
 }
