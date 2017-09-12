982c985f8aff9bf44aaf698a5a35f24b56d1d064
AVAVT <itsavavt@gmail.com>
2017-09-12 16:50:15 +0700

    Color Extension Methods

---
+++ ExtensionMethods/ColorExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public static class ColorExtensionMethods {
+    public static Color WithR(this Color c, float r)
+    {
+        return new Color(r, c.g, c.b, c.a);
+    }
+
+    public static Color WithG(this Color c, float g)
+    {
+        return new Color(c.r, g, c.b, c.a);
+    }
+
+    public static Color WithB(this Color c, float b)
+    {
+        return new Color(c.r, c.g, b, c.a);
+    }
+
+    public static Color WithA(this Color c, float a)
+    {
+        return new Color(c.r, c.g, c.b, a);
+    }
+}
