0f17074bf8e31c3631f21154b9aedf98faf6c931
AVAVT <itsavavt@gmail.com>
2017-09-19 19:41:27 +0700

    Scene Transition

---
+++ SceneTransition/CameraMasker.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CameraMasker : MonoBehaviour
+{
+    public Material TransitionMaterial;
+    public Texture2D maskTexture;
+    public Texture2D unmaskTexture;
+    public float maskTime = 0.2f;
+
+    private void Awake()
+    {
+        TransitionMaterial.SetFloat("_Cutoff", 1);
+    }
+
+    private void Start()
+    {
+        Unmask();
+    }
+
+    public void MaskChangeScene(string sceneName)
+    {
+        TransitionMaterial.SetTexture("_TransitionTex", maskTexture);
+        StartCoroutine(MaskCoroutine(0f, 1f, sceneName));
+    }
+
+    public void Unmask()
+    {
+        TransitionMaterial.SetTexture("_TransitionTex", unmaskTexture);
+        StartCoroutine(MaskCoroutine(1f, 0f));
+    }
+
+    IEnumerator MaskCoroutine(float start, float end, string targetScene = null)
+    {
+        float time = 0;
+        while (time < maskTime)
+        {
+            TransitionMaterial.SetFloat("_Cutoff", Mathf.Lerp(start, end, time / maskTime));
+            time += Time.deltaTime;
+            yield return null;
+
+        }
+
+        TransitionMaterial.SetFloat("_Cutoff", end);
+        if (!string.IsNullOrEmpty(targetScene))
+        {
+            SceneManager.LoadScene(targetScene);
+        }
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (TransitionMaterial != null)
+            Graphics.Blit(src, dst, TransitionMaterial);
+    }
+}
