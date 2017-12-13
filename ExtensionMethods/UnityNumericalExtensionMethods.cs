24a7d6396c2b6a53afc07ce338a731027298621a
AVAVT <itsavavt@gmail.com>
2017-12-13 21:08:14 +0700

    Whitespaces

--- ExtensionMethods/UnityNumericalExtensionMethods.cs
+++ ExtensionMethods/UnityNumericalExtensionMethods.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public static class ExtensionMethods
 {
-    public static float LinearRemap(this float value,
-                                     float valueRangeMin, float valueRangeMax,
-                                     float newRangeMin, float newRangeMax)
-    {
-        return (value - valueRangeMin) / (valueRangeMax - valueRangeMin) * (newRangeMax - newRangeMin) + newRangeMin;
-    }
+  public static float LinearRemap(this float value,
+                                   float valueRangeMin, float valueRangeMax,
+                                   float newRangeMin, float newRangeMax)
+  {
+    return (value - valueRangeMin) / (valueRangeMax - valueRangeMin) * (newRangeMax - newRangeMin) + newRangeMin;
+  }
 
-    public static int WithRandomSign(this int value, float negativeProbability = 0.5f)
-    {
-        return Random.value < negativeProbability ? -value : value;
-    }
+  public static int WithRandomSign(this int value, float negativeProbability = 0.5f)
+  {
+    return Random.value < negativeProbability ? -value : value;
+  }
 }
