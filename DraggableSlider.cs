4a193184a24c0e458ce8566369a900902f0a5679
AVAVT <itsavavt@gmail.com>
2018-04-29 09:35:03 +0700

    Added OnClick

--- DraggableSlider.cs
+++ DraggableSlider.cs
@@ -2,11 +2,16 @@ using System;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DraggableSlider : Slider, IEndDragHandler
+public class DraggableSlider : Slider, IEndDragHandler, IPointerClickHandler
 {
   public Action<float> OnRelease;
   
   public void OnEndDrag(PointerEventData eventData)
+  {
+    if (OnRelease != null) OnRelease(value);
+  }
+
+  public void OnPointerClick(PointerEventData eventData)
   {
     if (OnRelease != null) OnRelease(value);
   }
