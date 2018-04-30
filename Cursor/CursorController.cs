6d718321ec871c4da20835fdff4d070166bce0b9
AVAVT <itsavavt@gmail.com>
2018-04-30 17:06:02 +0700

    Cursor

---
+++ Cursor/CursorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorController : MonoBehaviour
+{
+  public static CursorController Instance { get; private set; }
+  public Sprite defaultCursor;
+  public Sprite highlightCursor;
+  public Sprite disabledCursor;
+
+  Image cursorImage;
+  private void Awake()
+  {
+    Instance = this;
+    cursorImage = GetComponent<Image>();
+  }
+  void Start()
+  {
+    UseDefaultCursor();
+  }
+
+  private void Update()
+  {
+    (transform as RectTransform).anchoredPosition = (
+      Input.mousePosition / GamePlayController.Instance.Services.gameWorldResizerService.Scale
+    ).xy();
+  }
+
+  public void UseDefaultCursor()
+  {
+    Cursor.visible = false;
+    cursorImage.sprite = defaultCursor;
+  }
+
+  public void UseHighLightCursor()
+  {
+    Cursor.visible = false;
+    cursorImage.sprite = highlightCursor;
+  }
+
+  public void UseDisabledCursor()
+  {
+    Cursor.visible = false;
+    cursorImage.sprite = disabledCursor;
+  }
+}
---
+++ Cursor/CursorInterator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CursorInterator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+  public Selectable targetSelectable;
+  private void Start()
+  {
+    if (targetSelectable == null) targetSelectable = GetComponent<Selectable>();
+  }
+  public void OnPointerEnter(PointerEventData eventData)
+  {
+    if (targetSelectable.interactable)
+    {
+      CursorController.Instance?.UseHighLightCursor();
+    }
+    else
+    {
+      CursorController.Instance?.UseDisabledCursor();
+    }
+  }
+
+  public void OnPointerExit(PointerEventData eventData)
+  {
+    CursorController.Instance?.UseDefaultCursor();
+  }
+}
