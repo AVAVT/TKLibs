a06b2b634b07bdb445dfe246b431e19b44a5acd7
AVAVT <itsavavt@gmail.com>
2018-05-02 18:12:09 +0700

    Alpha hit test

---
+++ EnableAlphaHitThreshold.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnableAlphaHitThreshold : MonoBehaviour
+{
+  public float alphaThreshold = 0.1f;
+  void Update()
+  {
+    GetComponent<Image>().alphaHitTestMinimumThreshold = alphaThreshold;
+  }
+}
