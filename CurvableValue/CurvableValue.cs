aae6177c7a5c6daa1c5f19d271d7a160dfa581e0
AVAVT <itsavavt@gmail.com>
2018-05-17 04:10:40 +0700

    nameChange

--- CurvableValue/CurvableValue.cs
+++ CurvableValue/CurvableValue.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 public class CurvableValue
 {
   [SerializeField] public AnimationCurve curve;
-  [SerializeField] public float min;
-  [SerializeField] public float max;
+  [SerializeField] public float minValue;
+  [SerializeField] public float maxValue;
   [SerializeField] public float start;
   [SerializeField] public float end;
   [SerializeField] public bool zeroBelowStart;
 
   public float ValueAt(float x)
   {
-    return (zeroBelowStart && x < start) ? 0 : TKUtils.ValueFromCurve(curve, (x - start) / (end - start), min, max);
+    return (zeroBelowStart && x < start) ? 0 : TKUtils.ValueFromCurve(curve, (x - start) / (end - start), minValue, maxValue);
   }
 
   public int IntValueAt(float x)
   {
-    return (zeroBelowStart && x < start) ? 0 : TKUtils.IntValueFromCurve(curve, (x - start) / (end - start), min, max);
+    return (zeroBelowStart && x < start) ? 0 : TKUtils.IntValueFromCurve(curve, (x - start) / (end - start), minValue, maxValue);
   }
 }
