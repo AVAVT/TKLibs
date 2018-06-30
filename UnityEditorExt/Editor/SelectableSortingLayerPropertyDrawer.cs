3be7e313581820627cec9f125409826adfed4416
AVAVT <itsavavt@gmail.com>
2018-07-01 05:00:18 +0700

    fix editor error

--- UnityEditorExt/Editor/SelectableSortingLayerPropertyDrawer.cs
+++ UnityEditorExt/Editor/SelectableSortingLayerPropertyDrawer.cs
@@ -2,34 +2,57 @@ using UnityEngine;
 using UnityEditor;
 using System;
 using System.Reflection;
+using UnityEditorInternal;
 
 [CustomPropertyDrawer(typeof(SelectableSortingLayerAttribute))]
 public class SelectableSortingLayerPropertyDrawer : PropertyDrawer
 {
+  private static string[] choices;
+  private static string[] Choices
+  {
+    get
+    {
+      if (choices == null)
+      {
+        choices = GetSortingLayerNames();
+      }
+
+      return choices;
+    }
+  }
+
   public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
   {
     SelectableSortingLayerAttribute selectableSortingLayerAttribute = (SelectableSortingLayerAttribute)attribute;
 
     if (property.propertyType == SerializedPropertyType.String)
     {
-      int index = Array.IndexOf(SelectableSortingLayerAttribute.Choices, property.stringValue);
+      int index = Array.IndexOf(Choices, property.stringValue);
 
       if (index < 0)
       {
-        Debug.LogWarning("WARNING: value" + property.stringValue + " of " + property.displayName + " is no longer available. Automatically using first value " + SelectableSortingLayerAttribute.Choices[0]);
+        Debug.LogWarning("WARNING: value" + property.stringValue + " of " + property.displayName + " is no longer available. Automatically using first value " + Choices[0]);
         index = 0;
       }
-      index = EditorGUI.Popup(position, property.displayName, index, SelectableSortingLayerAttribute.Choices);
+      index = EditorGUI.Popup(position, property.displayName, index, Choices);
 
-      property.stringValue = SelectableSortingLayerAttribute.Choices[index];
+      property.stringValue = Choices[index];
     }
     else if (property.propertyType == SerializedPropertyType.Integer)
     {
-      property.intValue = EditorGUI.Popup(position, property.displayName, property.intValue, SelectableSortingLayerAttribute.Choices);
+      property.intValue = EditorGUI.Popup(position, property.displayName, property.intValue, Choices);
     }
     else
     {
       base.OnGUI(position, property, label);
     }
+  }
+
+  private static string[] GetSortingLayerNames()
+  {
+    Type internalEditorUtilityType = typeof(InternalEditorUtility);
+    PropertyInfo sortingLayersProperty = internalEditorUtilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
+    var sortingLayers = (string[])sortingLayersProperty.GetValue(null, new object[0]);
+    return sortingLayers;
   }
 }
--- UnityEditorExt/SelectableSortingLayerAttribute.cs
+++ UnityEditorExt/SelectableSortingLayerAttribute.cs
@@ -2,25 +2,8 @@ using UnityEngine;
 using System;
 using System.Collections;
 using System.Reflection;
-using UnityEditorInternal;
+
 
 public class SelectableSortingLayerAttribute : PropertyAttribute
 {
-  private static string[] choices;
-  public static string[] Choices
-  {
-    get
-    {
-      if (choices == null) choices = GetSortingLayerNames();
-      return choices;
-    }
-  }
-
-  private static string[] GetSortingLayerNames()
-  {
-    Type internalEditorUtilityType = typeof(InternalEditorUtility);
-    PropertyInfo sortingLayersProperty = internalEditorUtilityType.GetProperty("sortingLayerNames", BindingFlags.Static | BindingFlags.NonPublic);
-    var sortingLayers = (string[])sortingLayersProperty.GetValue(null, new object[0]);
-    return sortingLayers;
-  }
 }
